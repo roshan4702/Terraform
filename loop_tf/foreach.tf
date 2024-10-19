6c7196af05f64837527403893d877a808003ef84
Ubuntu <ubuntu@ip-172-31-40-190.eu-north-1.compute.internal>
2024-10-19 06:44:53 +0000

    looping arguments

---
+++ loop_tf/foreach.tf
@@ -0,0 +1,11 @@
+
+resource "local_file" "f2" {
+  filename = each.value
+  for_each = toset(var.f2)
+  content  = "looping"
+}
+variable "f2" {
+  type   = list(string)
+  default = ["abc", "xyz"]
+}
+
